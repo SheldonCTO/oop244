96a2c3961c411ead38793cab4de4a014869139f7
SZETO Ngai man <115551742+SheldonCTO@users.noreply.github.com>
2023-07-10 16:12:14 -0400

    Add files via upload

--- w6/TextFile.cpp
+++ w6/TextFile.cpp
@@ -31,28 +31,27 @@ namespace sdds
    }
    Line &Line::operator=(const char *lineValue)
    {
-      if (lineValue != nullptr)
-      {
-         delete[] m_value;
-         m_value = new char[strLen(lineValue) + 1];
-         strCpy(m_value, lineValue);
-      }
+      delete[] m_value;
+      m_value = new char[strLen(lineValue) + 1];
+      strCpy(m_value, lineValue);
       return *this;
    }
    Line::~Line()
    {
       delete[] m_value;
-      m_value = nullptr;
    }
    // private function
    //  set all to safe status
    void TextFile::setEmpty()
    {
-      // delete[] m_textLines;
-      m_textLines = nullptr;
-      // delete[] m_filename;
-      m_filename = nullptr;
-      m_noOfLines = 0;
+      if (bool() == false)
+      {
+         delete[] m_textLines;
+         delete[] m_filename;
+         m_textLines = nullptr;
+         m_filename = nullptr;
+         m_noOfLines = 0;
+      }
    }
 
    void TextFile::setFilename(const char *fname, bool isCopy)
@@ -60,7 +59,7 @@ namespace sdds
       // condition exp for  allocate member size.
       int len;
       delete[] m_filename;
-
+      m_filename = nullptr;
       if (!isCopy)
       {
          len = strLen(fname);
@@ -72,58 +71,56 @@ namespace sdds
       {
          len = strLen(fname) + 2;
          m_filename = new char[len + 1];
-         strcpy(m_filename, "C_");
+         strCpy(m_filename, "C_");
          strCat(m_filename, fname);
       }
    }
 
    void TextFile::setNoOfLines()
    {
-      //string function to grab the line
       int noOfLines = 0;
       string line;
-      //set new char for create local object "open"
-      ifstream open(m_filename);
-      if (open.is_open())
+      ifstream openObj(name());
+      if (openObj.is_open())
       {
-         while (!open.eof())
+         while (!openObj.eof())
          {
-            getline(open, line);
+            getline(openObj, line);
             noOfLines++;
          }
-         open.close();
       }
       if (noOfLines == 0)
       {
+         delete[] m_filename;
          m_filename = nullptr;
       }
-      m_noOfLines = noOfLines;
-   
+      else
+      {
+         m_noOfLines = (noOfLines + 1);
+      }
+      openObj.close();
    }
 
    void TextFile::loadText()
+
    {
-      int i = 0;
-      string line;
-      // only active when m_filename is valid
-      if (m_filename != NULL)
+      // set bool check, only action if m_filename is not empty.
+      bool check = (m_filename == nullptr);
+      if (!check)
       {
-         // make m_textLines in safe before allocate
          delete[] m_textLines;
          m_textLines = new Line[m_noOfLines];
-         ifstream open(m_filename);
-         // using string and getline to find the length of each line
-         // read the file until the EOF
-         if (open.is_open())
+         ifstream openObj(m_filename);
+         unsigned int i = 0;
+         if (openObj.is_open())
          {
-            while (!open.eof())
+            string line;
+            while (getline(openObj, line))
             {
-               getline(open, line);
-               open >> line;
                m_textLines[i] = line.c_str();
                i++;
             }
-            open.close();
+            openObj.close();
          }
          m_noOfLines = i;
       }
@@ -131,32 +128,38 @@ namespace sdds
 
    void TextFile::saveAs(const char *fileName) const
    {
-      ofstream open(fileName);
+
+      ofstream openObj(fileName);
       // read the local object OPEN to loop the textLine array by noOfLine
-      if (open.is_open())
+      if (openObj.is_open())
       {
-         for (int i = 0; i < m_noOfLines; i++)
+         for (unsigned int i = 0; i < m_noOfLines; i++)
          {
             // add 1 line to the end each line.
-            open << m_textLines[i] << endl;
+            openObj << m_textLines[i] << endl;
          }
       }
-      open.close();
+      openObj.close();
    }
 
    TextFile::TextFile(unsigned pageSize)
    {
       // set empty and attribute using pageSize = 15
-      setEmpty();
+      m_textLines = nullptr;
+      m_filename = nullptr;
+      m_noOfLines = 0;
       m_pageSize = pageSize;
    }
 
    TextFile::TextFile(const char *filename, unsigned pageSize)
    {
       // set empty and attribute using pageSize = 15 and current filename
-      setEmpty();
+
+      m_textLines = nullptr;
+      m_filename = nullptr;
+      m_noOfLines = 0;
       m_pageSize = pageSize;
-      if (filename != nullptr)
+      if (filename)
       {
          setFilename(filename);
          setNoOfLines();
@@ -167,12 +170,14 @@ namespace sdds
    // copy construct
    TextFile::TextFile(const TextFile &src)
    {
-      setEmpty();
+      m_textLines = nullptr;
+      m_filename = nullptr;
+      m_noOfLines = 0;
       m_pageSize = src.m_pageSize;
       if (src)
       {
-         setFilename(src.m_filename, true);
-         saveAs(src.m_filename);
+         setFilename(src.name(), true);
+         src.saveAs(name());
          setNoOfLines();
          loadText();
       }
@@ -181,23 +186,28 @@ namespace sdds
    // copy assignment
    TextFile &TextFile::operator=(const TextFile &src)
    {
+      if (this != &src)
+      {
+         delete[]m_textLines;
+         m_textLines = nullptr;
+         m_noOfLines = 0;
+         m_pageSize = src.m_pageSize;
          if (src)
          {
-            delete[] m_textLines;
-            m_textLines = nullptr;
-            // m_pageSize = src.m_pageSize;
-            src.saveAs(m_filename);
+            src.saveAs(name());
             setNoOfLines();
             loadText();
          }
+      }
       return *this;
    }
 
-   // destruct
    TextFile::~TextFile()
    {
-      delete[] m_filename;
       delete[] m_textLines;
+      m_textLines = nullptr;
+      delete[] m_filename;
+      m_filename = nullptr;
    }
 
    unsigned TextFile::lines() const
@@ -205,88 +215,82 @@ namespace sdds
       return m_noOfLines;
    }
 
-   // display, use iomanip library
    std::ostream &TextFile::view(std::ostream &ostr) const
    {
       int fileNameLength = strLen(m_filename);
-      ifstream open(m_filename);
-      if (*this)
+      ifstream openObj(m_filename);
+      if (*this && m_noOfLines >= 1)
       {
+         //using iomanip
          ostr << m_filename << endl;
          ostr << setfill('=');
          ostr << setw(fileNameLength) << "=" << endl;
          ostr << setfill(' ');
 
-         int i = 0;
-         if (open.is_open())
+         unsigned int i = 0;
+         if (openObj.is_open())
          {
             std::string line;
-            while (getline(open, line))
+            while (getline(openObj, line))
             {
                ostr << line.c_str() << endl;
                i++;
                if (i % m_pageSize == 0)
                {
-
                   ostr << "Hit ENTER to continue...";
                   cin.ignore(1000, '\n');
                }
             }
-            open.close();
+            openObj.close();
          }
       }
       return ostr;
    }
-   // receive file
+
    std::istream &TextFile::getFile(std::istream &istr)
    {
-      // create local filename for receiving the file
-
-      char *fileName = new char[MAX_PATH + 1];
-      istr.getline(fileName, MAX_PATH + 1);
-      setFilename(fileName);
+      string objName;
+      istr >> objName;
+      cin.ignore();
+      setFilename(objName.c_str());
       setNoOfLines();
       loadText();
-      // delete[] fileName;
+
       return istr;
    }
 
    const char *TextFile::operator[](unsigned index) const
    {
+
       const char *element = nullptr;
       if (*this)
       {
-         if (index >= m_noOfLines) // if index = 10, noOfLine = 10 ,
-         {
-            // return element 10 -10 = index:0 <-first element
-            // if index = 11, return   index:1 <-second element
-            index = index - m_noOfLines;
-
-            element = m_textLines[index];
-         }
+         // return element 10 or 20  % 10 = index:0 <-first element
+         // if index = 11, return   index:1 <-second element
+         index = index % m_noOfLines;
+         element = m_textLines[index];
       }
       return element;
-   }
-
-   TextFile::operator bool() const
-   {
-      return (m_filename != nullptr && m_textLines != nullptr && m_noOfLines != 0);
    }
 
    const char *TextFile::name() const
    {
       return m_filename;
    }
-
-   std::ostream &operator<<(std::ostream &ostr, const TextFile &text)
+   TextFile::operator bool() const
    {
-      // just print the ostr
-      text.view(ostr);
-      return ostr;
+      bool check = true;
+      check = (m_filename == nullptr && m_textLines == nullptr && m_noOfLines == 0) ? false : true;
+      return check;
    }
-   std::istream &operator>>(std::istream &istr, TextFile &text)
+
+   ostream &operator<<(ostream &ostr, const TextFile &text)
+   {
+      return text.view(ostr);
+   }
+
+   istream &operator>>(istream &istr, TextFile &text)
    {
       return text.getFile(istr);
    }
 }
-
